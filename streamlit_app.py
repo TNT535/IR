5a9140a4312cb48aac4c109a5bb4f71b5bda589e
dxmai <19521820@gm.uit.edu.vn>
2022-01-20 15:47:17 +0700

    input

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-user_input = st.text_input('Nhập từ khoá muốn tìm kiếm')
-user_input
+st.set_page_config(page_title='IR System', page_icon=':mag')
+st.title("Hệ thống tìm kiếm tin tức bằng tiếng Việt")
+user_input = st.text_input('Nhập từ khoá muốn tìm kiếm', 'Nhập')
+clicked = st.button('Search')
+if user_input == '':
+    st.error("Hãy nhập từ khoá cần tìm kiếm")
+else:
+    st.write("Từ khoá là", user_input)
