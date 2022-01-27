e1f56179ef23264611e935e8beaf1fe6483f3016
dxmai <19521820@gm.uit.edu.vn>
2022-01-27 15:08:25 +0700

    requirements setting

--- helper.py
+++ helper.py
@@ -7,8 +7,8 @@ from scipy import sparse
 from underthesea import word_tokenize
 
 
-# nltk.download('punkt')
-# nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('stopwords')
 
 with open('vietnamese-stopwords-dash.txt', mode='r', encoding='utf-8') as f:
     stop_words = set(f.read().split())
